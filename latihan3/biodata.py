5e9bdaf320823fad05125bcb054359c47aff66ba
Asepsukandar22 <asep.sukandar_ti23@nusaputra.ac.id>
2024-03-09 15:21:18 +0700

    Penambahan biodata.py

---
+++ latihan3/biodata.py
@@ -0,0 +1,18 @@
+# membuat variabel
+nama = "Asep Sukandar"
+semester = 2
+kelas = "TI23B"
+jurusan = "TEKNIK INFORMATIKA"
+quete = "my hoby is coding"
+married = True
+adalahPerempuan = False
+hobi = ["Nonton", "Main Game","Tidur"]
+
+print(nama)
+print(semester)
+print(kelas)
+print(jurusan)
+print(quete)
+print(married)
+print(adalahPerempuan)
+print(hobi)
